e31e4d687bdfea13457636c793f655c8066a0476
John Miller <johnpmiller16@gmail.com>
2017-08-18 15:47:19 -0700

    Add augmented dataset

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Standard dataset
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O train.json
 wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O dev.json
+
+# Unzip compressed dataset
+zip -s 0 augmented_data/augmented_zips.zip --out augmented_train.json.zip
+unzip augmented_train.json.zip
